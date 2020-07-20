44b7dd5d32104b905327cadb7e969c3d87cf1df3
xyphoz-apt <68557596+xyphoz-apt@users.noreply.github.com>
2020-07-20 18:33:07 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let x = 5;
+const letwike = [2,5,"vetwike",x, [],{},function(){}, true]
+const objectiva = {
+  name: "mail",
+  age: 15 ,
+}
+letwike[1] = 5
+console.log(objectiva.name);
+// var, const, let
+// number, string, undefined, NaN, object, boolean, function
